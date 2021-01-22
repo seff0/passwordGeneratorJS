8b51d5ff6912356c793e4dc144fd070e6a1c05dd
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-21 19:39:43 -0700

    Feat for loop
    Add feature for loop in generatePassword function. The for loop pulls a random character from the charset array for as many characters as the user selected.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,8 @@
 var generateBtn = document.querySelector("#generate");
-var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialChars = ["!","@","#","$","%","^","&","*","(",")","_","+","-","=",",",".","?","/"]
-var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
 
 function writePassword() {
-  var characters  = prompt("How many characters?");
+  var characterPrompt  = prompt("How many characters?");
+  var characters = parseInt(characterPrompt);
   if ((characters<8 || characters>128)) {
     alert("Password must be between 8-128 characters.");
     writePassword();
@@ -33,13 +29,12 @@ function writePassword() {
     }
     if (numbers) {
       charset.splice(0,0, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+    } 
+    var finishedPassword = "";
+    for (i = 0; i < characters; i++) {
+      finishedPassword += charset[Math.floor(Math.random() * charset.length)];
     }
-    console.log(
-      charset
-    )
-    // for (i = 0; i < characters; i++) {
-
-    // }
+    return finishedPassword;
   }
 
   var password = generatePassword();
