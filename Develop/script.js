6d17089aa31d028d7b26930fbf6e638f21b2a2d8
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-21 18:30:54 -0700

    Add arrays
    Add arrays for the password generator to pull characters from

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialChars = ["!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 // Write password to the #password input
 function writePassword() {
   var characters  = prompt("How many characters?");
