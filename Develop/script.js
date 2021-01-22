2d4f435a4db490cb59702c120b657d3799045131
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-21 19:49:51 -0700

    Comment script.js
    Comment the script.js file to better communicate the point of each snippet of code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,30 @@
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
+  //Ask the user how many characters
   var characterPrompt  = prompt("How many characters?");
+  //Prompt returns a string, so use parseInt to convert that string to an integer
   var characters = parseInt(characterPrompt);
+  //If less than 8 or more than 128 characters are selected, show alert and start over
   if ((characters<8 || characters>128)) {
     alert("Password must be between 8-128 characters.");
     writePassword();
   }
+  //These are the password criteria prompts. 
   var uppercase = confirm("Uppercase letters?");
   var lowercase = confirm("Lowercase letters?");
   var specials = confirm("Special characters?");
   var numbers = confirm("Numbers?");
+  //If all 4 criteria prompts are falsy, show alert and start over
   if (!uppercase && !lowercase && !specials && !numbers) {
     alert("Password must include at least one type of character, silly.")
     writePassword()
   }
-
+  //Take the user inputs and make a password out of them
   function generatePassword() {
-    var charset = []
+    //Make an empty array
+    var charset = [];
+    //If user selected uppercase, splice all uppercase characters into the charset array
     if (uppercase) {
       charset.splice(0,0, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
     }
@@ -30,13 +37,18 @@ function writePassword() {
     if (numbers) {
       charset.splice(0,0, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     } 
+    //Array is finished, now time to pull from it
+    //Make an empty string to put out characters into
     var finishedPassword = "";
+    //For loop will choose a random character from charset and insert it into finishedPassword until user-defined character limit is reached
     for (i = 0; i < characters; i++) {
       finishedPassword += charset[Math.floor(Math.random() * charset.length)];
     }
+    //Finally, return finishedPassword to be inserted into HTML
     return finishedPassword;
   }
-
+  
+  //These last few lines were provided for this assignment
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
