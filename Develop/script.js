b0a2040b4473926da70eab33b4143afa80686ff3
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-21 18:29:31 -0700

    Feat writePassword
    Create variables and methods for the writePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var characters  = prompt("How many characters?");
+  if ((characters<8 || characters>128)) {
+    alert("Password must be between 8-128 characters.");
+    writePassword();
+  }
+  var uppercase = confirm("Uppercase letters?");
+  var lowercase = confirm("Lowercase letters?");
+  var specials = confirm("Special characters?");
+  var numbers = confirm("Numbers?");
+  if (!uppercase && !lowercase && !specials && !numbers) {
+    alert("Password must include at least one type of character, silly.")
+    writePassword()
+  }
+  var password = generatePassword() {
+    
+  };
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
